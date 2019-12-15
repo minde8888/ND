e516edfb2692a9049483f18ea011f3bad2dd9247
minde bal <mindebaltru@gmail.com>
2019-12-15 21:15:13 +0100

    baigtas numu darbas

--- loginis_1/loginis_1.php
+++ loginis_1/loginis_1.php
@@ -1,13 +1,26 @@
 <?php
-$a = 'lietuva grazi salis';
-/*$a = ' vienas. du;trys';*/
+/*$a = 'lietuva grazi salis';*/
+$a = ' vienas. du;trys';
+$b = "";
+$b = $a;
+
 for ($i=0; $i<strlen($a); $i++){
 	if ($i==0){
-		echo strtoupper($a[0]);
-	} else if ($a[$i] == ' '){ 
+		substr($a, 0, 9);
+		echo strtoupper($a[1]);
+		$i++;
+	}else if ($a[$i] == '.'){ 
+		echo '';	
+	}else if ($a[$i] == ';'){
+		echo strtoupper(" ".$a[$i+1]);
+		$i++;
+	}else if ($a[$i] == ' '){ 
 		echo ' ';
 		echo strtoupper($a[$i+1]);
-		$i++;
-	} else 
+	}else 
 		echo $a[$i];
+		
 }
+
+
+
