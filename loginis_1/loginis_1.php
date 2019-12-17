707e2368c0852804f03f606522d371690e122ca6
minde bal <mindebaltru@gmail.com>
2019-12-17 19:47:52 +0100

    1 patobulinimas

--- loginis_1/loginis_1.php
+++ loginis_1/loginis_1.php
@@ -1,26 +1,37 @@
 <?php
-/*$a = 'lietuva grazi salis';*/
-$a = ' vienas. du;trys';
-$b = "";
-$b = $a;
-
-for ($i=0; $i<strlen($a); $i++){
-	if ($i==0){
-		substr($a, 0, 9);
-		echo strtoupper($a[1]);
-		$i++;
-	}else if ($a[$i] == '.'){ 
-		echo '';	
-	}else if ($a[$i] == ';'){
-		echo strtoupper(" ".$a[$i+1]);
-		$i++;
-	}else if ($a[$i] == ' '){ 
-		echo ' ';
-		echo strtoupper($a[$i+1]);
-	}else 
-		echo $a[$i];
-		
+class TextClass {
+	public static function sakinys ($text){
+		$result = '';
+		for ($i=0; $i<strlen($text); $i++){
+			if ($i==0 ){
+				if ($text[$i] == ' '){ 
+					$result .= ' ';
+					$i++;
+				}
+				$result .= strtoupper($text[$i]);
+			}else if ($text[$i] == ';'){
+				$result .= strtoupper(";".$text[$i+1]);
+				$i++;
+			}else if ($text[$i] == ' '){ 
+				$result .= ' ';
+				$result .= strtoupper($text[$i+1]);
+				$i++;
+			}else 
+				$result .= $text[$i];
+		}
+		return $result;
+	}
 }
+
+$test1 = TextClass::sakinys('lietuva grazi salis');
+echo strcmp($test1, 'Lietuva Grazi Salis');
+$test2 = TextClass::sakinys(' vienas. du;trys');
+echo strcmp($test2, ' Vienas. Du;Trys');
+$test3 = TextClass::sakinys('    oslas norvegijos  sostine');
+echo strcmp($test3, '    Oslas Norvegijos  Sostine');
+echo $test3;
+
+
 
 
 
